ccc660a1a9165e19f74694dff6593c7096ccc6b3
Simon Hirtreiter <simon.hirtreiter@hotmail.de>
2024-07-18 08:56:23 +0200

    Feature/gateway (#3)
    * init gateway from internal archtype
    
    * gateway fix package
    
    * mv build config to root
    
    * cp build from digiwf
    
    * gateway fix maven filters in config
    
    * gateway upgrade hazlecast
    
    * gateway upgrade hazlecast
    
    * upgrade dependencies
    
    * gateway reformat tests
    
    * gateway update config
    
    * gateway add csrf whitelist
    
    * gateway update dependency, cleanup config and add Dockerfile
    
    * gateway update no-security syntax
    
    * gateway update security config
    
    * gateway add csrf whitelist
    
    * gateway fiy warnings and format files
    
    * update ci
    
    * gateway init README.md
    
    * init ci
    
    * init ci
    
    * init ci
    
    * Revert "init ci"
    
    This reverts commit e471c62d93d988df43959a7e4f49c110e69b75cb.
    
    * updaten maven version enforce
    
    * ci use github dependency review
    
    * init README.md
    
    * ci github dependency review run for all prs
    
    * ci maven build refactor token usage
    
    * Revert "init ci"
    
    This reverts commit fcc4abace0469e3cc3595e1468b0b03bdf5be838.
    
    * ci fix naming
    
    * ci fix naming
    
    * ci fix naming
    
    * ci fix naming
    
    * ci maven update names
    
    * ci maven rename
    
    * ci maven rename
    
    * gateway rm frontend configurations
    
    * dependencies use spring boot 3.3.1 and cloud 2023.0.2
    
    * ci use matrix for image build
    
    * ci use matrix for image build
    
    * ci add release logic
    
    * rm duplicate .gitignore
    
    * rm unneeded start scripts
    
    * gateway rm copyright notice
    
    * gateway replace hazlecast codec dependency
    
    * gateway fix typo
    
    * refactor maven build config
    
    * refactor maven build config and reformat
    
    * use hazelcast 5.3.6

---
+++ refarch-gateway/src/main/java/de.muenchen.oss.refarch.gateway/configuration/SecurityProperties.java
@@ -0,0 +1,16 @@
+package de.muenchen.oss.refarch.gateway.configuration;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties("refarch.security")
+public class SecurityProperties {
+    /**
+     * List of url patterns excluded from csrf protection.
+     */
+    private List<String> csrfWhitelisted = List.of();
+}
