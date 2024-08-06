b3735718f24c3a0f5d223b7a14eaed6abad1d8f1
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2024-08-06 10:00:24 +0200

    :sparkles: email-integration init starter

---
+++ refarch-integrations/refarch-email-integration/refarch-email-integration-starter/src/main/java/de/muenchen/refarch/email/integration/configuration/MailAutoConfiguration.java
@@ -0,0 +1,48 @@
+package de.muenchen.refarch.email.integration.configuration;
+
+import de.muenchen.refarch.email.api.DigiwfEmailApi;
+import de.muenchen.refarch.email.integration.adapter.out.mail.MailAdapter;
+import de.muenchen.refarch.email.integration.adapter.out.s3.S3Adapter;
+import de.muenchen.refarch.email.integration.application.port.in.SendMailPathsInPort;
+import de.muenchen.refarch.email.integration.application.port.out.LoadMailAttachmentOutPort;
+import de.muenchen.refarch.email.integration.application.port.out.MailOutPort;
+import de.muenchen.refarch.email.integration.application.usecase.SendMailPathsUseCase;
+import de.muenchen.refarch.s3.integration.client.repository.DocumentStorageFileRepository;
+import de.muenchen.refarch.s3.integration.client.repository.DocumentStorageFolderRepository;
+import de.muenchen.refarch.s3.integration.client.repository.transfer.S3FileTransferRepository;
+import de.muenchen.refarch.s3.integration.client.service.FileService;
+import de.muenchen.refarch.s3.integration.client.service.S3StorageUrlProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties({ MailProperties.class })
+public class MailAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SendMailPathsInPort getSendMailPathsInPort(final LoadMailAttachmentOutPort loadAttachmentPort, final MailOutPort mailOutPort) {
+        return new SendMailPathsUseCase(loadAttachmentPort, mailOutPort);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public LoadMailAttachmentOutPort getLoadMailAttachmentPort(final S3FileTransferRepository s3FileTransferRepository,
+            final DocumentStorageFileRepository documentStorageFileRepository,
+            final DocumentStorageFolderRepository documentStorageFolderRepository,
+            final FileService fileService,
+            final S3StorageUrlProvider s3DomainService) {
+        return new S3Adapter(s3FileTransferRepository, documentStorageFileRepository, documentStorageFolderRepository, fileService, s3DomainService);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public MailOutPort getMailPort(final DigiwfEmailApi digiwfEmailApi) {
+        return new MailAdapter(digiwfEmailApi);
+    }
+}
