6f3ff7fb7d0b6685e359be382d48f2f836cf13ee
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2024-08-09 08:52:57 +0200

    :fire: s3-integration rm unneeded validation annotation

--- refarch-integrations/refarch-s3-integration/refarch-s3-integration-client-starter/src/main/java/de/muenchen/refarch/s3/integration/client/properties/S3IntegrationClientProperties.java
+++ refarch-integrations/refarch-s3-integration/refarch-s3-integration-client-starter/src/main/java/de/muenchen/refarch/s3/integration/client/properties/S3IntegrationClientProperties.java
@@ -5,11 +5,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.util.unit.DataSize;
-import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
-@Validated
 @ConfigurationProperties(prefix = "refarch.s3.client")
 public class S3IntegrationClientProperties {
 
