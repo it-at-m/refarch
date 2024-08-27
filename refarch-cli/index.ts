b386f11d94d481ef7ec8244126db1330faa46010
DanielOber <145556033+DanielOber@users.noreply.github.com>
2024-08-27 14:47:09 +0200

    code fixes

--- refarch-cli/index.ts
+++ refarch-cli/index.ts
@@ -1,39 +1,41 @@
 #!/usr/bin/env node
 import * as fs from "fs";
 import {replaceInFile} from "replace-in-file";
-import {checkbox, input, select} from "@inquirer/prompts";
-
+import {input, select, Separator} from "@inquirer/prompts";
 
 async function startpoint() {
     const FRONTEND = "frontend";
     const BACKEND = "backend";
     const EAI = "eai";
+    const EXIT = "exit"
     await select({
         message: 'Select Project/s you want to generate with space',
         choices: [
-            {name: FRONTEND, value: FRONTEND, description: ""},
-            {name: BACKEND, value: BACKEND, description: ""},
-            {name: EAI, value: EAI, description: ""},
-            {name: "exit", value: "EXIT"}
+            {name: FRONTEND, value: FRONTEND},
+            {name: BACKEND, value: BACKEND},
+            {name: EAI, value: EAI},
+            {name: EXIT, value: EXIT},
+            new Separator()
         ]
     }).then(async (result: string) => {
-        if (result == BACKEND) {
-            await generateBackend();
+        switch (result) {
+            case BACKEND:
+                await generateBackendInteractiveCli();
+                break;
+            case FRONTEND:
+                await generateFrontendInteractiveCli();
+                break;
+            case EAI:
+                await generateEaiInteractiveCli();
+                break;
         }
-        if (result == FRONTEND) {
-            await generateFrontend();
-        }
-        if (result == EAI) {
-            await generateEAI();
-        }
-        if (result != "EXIT") {
+        if (result != EXIT) {
             await startpoint();
         }
     });
 }
 
-async function generateBackend() {
-    fs.cpSync("../refarch-backend", "../refarch-backend-copy", {recursive: true});
+async function generateBackendInteractiveCli() {
     const groupId = await input({
         message: "Define value for property groupId (should match expression '^de\\.muenchen\\.[a-z0-9]+(\\.[a-z0-9]+)*$'): ",
         validate(value) {
@@ -42,8 +44,9 @@ async function generateBackend() {
         },
         required: true,
     });
-    const artifactId = await input({message: "Define value for property artifactId:",  required: true,});
-    const packageName = await input({message: "Define value for property package:",
+    const artifactId = await input({message: "Define value for property artifactId:", required: true,});
+    const packageName = await input({
+        message: "Define value for property package:",
         default: groupId,
         validate(value) {
             const pass = value.match(/^de\.muenchen\.[a-z0-9]+(\.[a-z0-9]+)*$/g);
@@ -51,7 +54,13 @@ async function generateBackend() {
         },
         required: true,
     });
-    const projectName = await input({message: "Define value for Project Name:",  required: true});
+    const projectName = await input({message: "Define value for Project Name:", required: true});
+    generateBackend(packageName, groupId, artifactId, projectName);
+}
+
+
+function generateBackend(packageName, groupId, artifactId, projectName) {
+    fs.cpSync("../refarch-backend", "../refarch-backend-copy", {recursive: true});
     const replacements = [{
         files: "../refarch-backend-copy/src/main/java/de/muenchen/refarch/**/*.java",
         from: [/package de.muenchen.refarch/g, /import de.muenchen.refarch/g],
@@ -68,14 +77,16 @@ async function generateBackend() {
     Promise.all(
         replacements.map(options => replaceInFile(options))
     ).then(result => {
-        console.log(result)
-        fs.rename("../refarch-backend-copy", `../${projectName}`, () => {});
+        fs.renameSync("../refarch-backend-copy", `../${projectName}`);
     });
-    return Promise<void>;
 }
 
-async function generateFrontend() {
-    const name = await input({message: "Define value for property name:",  required: true,});
+async function generateFrontendInteractiveCli() {
+    const name = await input({message: "Define value for property name:", required: true,});
+    await generateFrontend(name)
+}
+
+async function generateFrontend(name: string) {
     const replacements = {
         files: ["../refarch-frontend/package.json", "../refarch-frontend/package-lock.json"],
         from: [/refarch-frontend/g],
@@ -87,7 +98,7 @@ async function generateFrontend() {
     console.log(result);
 }
 
-async function generateEAI() {
+async function generateEaiInteractiveCli() {
     const groupId = await input({
         message: "Define value for property groupId (should match expression '^de\\.muenchen\\.[a-z0-9]+(\\.[a-z0-9]+)*$'): ",
         validate(value) {
@@ -96,8 +107,9 @@ async function generateEAI() {
         },
         required: true,
     });
-    const artifactId = await input({message: "Define value for property artifactId:",  required: true,});
-    const packageName = await input({message: "Define value for property package:",
+    const artifactId = await input({message: "Define value for property artifactId:", required: true,});
+    const packageName = await input({
+        message: "Define value for property package:",
         default: groupId,
         validate(value) {
             const pass = value.match(/^de\.muenchen\.[a-z0-9]+(\.[a-z0-9]+)*$/g);
@@ -105,14 +117,19 @@ async function generateEAI() {
         },
         required: true,
     });
+    await generateEAI(groupId, artifactId, packageName);
+}
+
+async function generateEAI(packageName, groupId, artifactId,) {
+    fs.cpSync("../refarch-eai", "../refarch-eai-copy", {recursive: true});
     const replacements = [{
-        files: "../refarch-eai/src/main/java/de/muenchen/refarch/**/*.java",
+        files: "../refarch-eai-copy/src/main/java/de/muenchen/refarch/**/*.java",
         from: [/package de.muenchen.refarch/g, /import de.muenchen.refarch/g],
         to: [`package ${packageName}`, `import ${packageName}`],
         dry: true,
         countMatches: true,
     }, {
-        files: "../refarch-eai/pom.xml",
+        files: "../refarch-eai-copy/pom.xml",
         from: ["<groupId>de.muenchen.refarch</groupId>", "<artifactId>refarch-eai</artifactId>", "<name>refarch_eai</name>"],
         to: [`<groupId>${groupId}</groupId>`, `<artifactId>${artifactId}</artifactId>`, `<name>${artifactId}</name>`],
         dry: true,
@@ -121,9 +138,14 @@ async function generateEAI() {
     Promise.all(
         replacements.map(options => replaceInFile(options))
     ).then(result => {
-        console.log(result)
-       // fs.rename("../refarch-eai", `../${projectName}`, () => {});
+        fs.renameSync("../refarch-eai-copy", `../${artifactId}`);
     });
+}
+
+function cleanup() {
+    fs.rmSync("../refarch-eai", {recursive: true})
+    fs.rmSync("../refarch-backend", {recursive: true})
+    fs.rmSync("../refarch-frontend", {recursive: true})
 }
 
 module.exports = startpoint();
