46aa9993add6e426e7f247547d5c699c5720b326
Simon Hirtreiter <14137977+simonhir@users.noreply.github.com>
2024-10-15 13:12:09 +0200

    Maint/hazelcast properties (#206)
    * :recycle: gateway refactor hazelcast properties
    
    * :memo: gateway document hazelcast properties
    
    * :memo: gateway document hazelcast module java properties
    
    * :memo: gateway document hazelcast kubernetes
    
    * :art: gateway sportless format
    
    * :recycle: gateway rm hazelcast kubernetes namespace as auto discovered
    
    * :recycle: gateway refactor properties load
    
    * :recycle: gateway refactor properties load

---
+++ refarch-gateway/src/main/java/de/muenchen/refarch/gateway/configuration/HazelcastProperties.java
@@ -0,0 +1,27 @@
+package de.muenchen.refarch.gateway.configuration;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@RequiredArgsConstructor
+@AllArgsConstructor
+@ConfigurationProperties("refarch.hazelcast")
+@SuppressWarnings("PMD.ImmutableField")
+public class HazelcastProperties {
+    /**
+     * Name of the hazelcast cluster.
+     */
+    private String clusterName = "session_replication_group";
+    /**
+     * Name of the hazelcast instance.
+     */
+    private String instanceName = "hazl_instance";
+    /**
+     * Kubernetes service name.
+     * Required for running hazelcast inside kubernetes.
+     */
+    private String serviceName;
+}
