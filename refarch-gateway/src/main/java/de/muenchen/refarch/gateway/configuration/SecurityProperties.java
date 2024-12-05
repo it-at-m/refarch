d1029954af720636e16dbce3edca823aa5fe30ea
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2024-12-05 15:53:48 +0100

    :bug: gateway fix csrf property not settable

--- refarch-gateway/src/main/java/de/muenchen/refarch/gateway/configuration/SecurityProperties.java
+++ refarch-gateway/src/main/java/de/muenchen/refarch/gateway/configuration/SecurityProperties.java
@@ -1,14 +1,16 @@
 package de.muenchen.refarch.gateway.configuration;
 
 import java.util.List;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@Data
 @ConfigurationProperties("refarch.security")
 public class SecurityProperties {
     /**
      * List of url patterns excluded from csrf protection.
      */
-    private final List<String> csrfWhitelisted = List.of();
+    private List<String> csrfWhitelisted = List.of();
 
     public List<String> getCsrfWhitelisted() {
         return List.copyOf(this.csrfWhitelisted);
