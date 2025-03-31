12345047eb96062347c57dae2b46267f2e30730a
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2025-03-31 11:30:10 +0200

    add unit test

--- refarch-integrations/refarch-s3-integration/refarch-s3-integration-rest/refarch-s3-integration-rest-client/src/test/java/de/muenchen/refarch/integration/s3/client/repository/DocumentStorageFolderRestRepositoryTest.java
+++ refarch-integrations/refarch-s3-integration/refarch-s3-integration-rest/refarch-s3-integration-rest-client/src/test/java/de/muenchen/refarch/integration/s3/client/repository/DocumentStorageFolderRestRepositoryTest.java
@@ -13,11 +13,17 @@ import de.muenchen.refarch.integration.s3.client.api.FolderApiApi;
 import de.muenchen.refarch.integration.s3.client.exception.DocumentStorageClientErrorException;
 import de.muenchen.refarch.integration.s3.client.exception.DocumentStorageException;
 import de.muenchen.refarch.integration.s3.client.exception.DocumentStorageServerErrorException;
+import de.muenchen.refarch.integration.s3.client.model.FileMetadataDto;
 import de.muenchen.refarch.integration.s3.client.model.FileSizesInFolderDto;
 import de.muenchen.refarch.integration.s3.client.model.FilesInFolderDto;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import de.muenchen.refarch.integration.s3.client.model.FilesMetadataInFolderDto;
 import de.muenchen.refarch.integration.s3.client.repository.mapper.FileMetadataMapperImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -99,6 +105,52 @@ class DocumentStorageFolderRestRepositoryTest {
         doThrow(new RestClientException("Something happened")).when(this.folderApi).getAllFilesInFolderRecursively(pathToFolder);
         assertThrows(DocumentStorageException.class, () -> this.documentStorageFolderRestRepository.getAllFilesInFolderRecursively(pathToFolder));
         verify(this.folderApi, times(1)).getAllFilesInFolderRecursively(pathToFolder);
+    }
+
+    @Test
+    void testGetMetadataOfAllFilesInFolderRecursively()
+            throws DocumentStorageException, DocumentStorageClientErrorException, DocumentStorageServerErrorException {
+        final String pathToFolder = "folder";
+
+        final FilesMetadataInFolderDto filesMetadataInFolderDto = new FilesMetadataInFolderDto();
+
+        final List<FileMetadataDto> files = new ArrayList<>();
+        FileMetadataDto fileMetadataDto = new FileMetadataDto();
+        fileMetadataDto.setFileSize(1001L);
+        fileMetadataDto.setPathToFile("/file1");
+        fileMetadataDto.setEtag("etag1");
+        files.add(fileMetadataDto);
+        fileMetadataDto.setLastModified(LocalDateTime.of(2025, 2, 1, 23, 59, 59));
+        fileMetadataDto = new FileMetadataDto();
+        fileMetadataDto.setFileSize(1002L);
+        fileMetadataDto.setPathToFile("/file2");
+        fileMetadataDto.setEtag("etag2");
+        fileMetadataDto.setLastModified(LocalDateTime.of(2025, 2, 2, 23, 59, 59));
+        files.add(fileMetadataDto);
+
+        filesMetadataInFolderDto.setFiles(files);
+
+        reset(this.folderApi);
+        when(this.folderApi.getMetadataOfAllFilesInFolderRecursively(pathToFolder)).thenReturn(Mono.just(filesMetadataInFolderDto));
+        this.documentStorageFolderRestRepository.getMetadataOfAllFilesInFolderRecursively(pathToFolder);
+        verify(this.folderApi, times(1)).getMetadataOfAllFilesInFolderRecursively(pathToFolder);
+
+        reset(this.folderApi);
+        doThrow(new HttpClientErrorException(HttpStatus.BAD_REQUEST)).when(this.folderApi).getMetadataOfAllFilesInFolderRecursively(pathToFolder);
+        assertThrows(DocumentStorageClientErrorException.class,
+                () -> this.documentStorageFolderRestRepository.getMetadataOfAllFilesInFolderRecursively(pathToFolder));
+        verify(this.folderApi, times(1)).getMetadataOfAllFilesInFolderRecursively(pathToFolder);
+
+        reset(this.folderApi);
+        doThrow(new HttpServerErrorException(HttpStatus.INTERNAL_SERVER_ERROR)).when(this.folderApi).getMetadataOfAllFilesInFolderRecursively(pathToFolder);
+        assertThrows(DocumentStorageServerErrorException.class,
+                () -> this.documentStorageFolderRestRepository.getMetadataOfAllFilesInFolderRecursively(pathToFolder));
+        verify(this.folderApi, times(1)).getMetadataOfAllFilesInFolderRecursively(pathToFolder);
+
+        reset(this.folderApi);
+        doThrow(new RestClientException("Something happened")).when(this.folderApi).getMetadataOfAllFilesInFolderRecursively(pathToFolder);
+        assertThrows(DocumentStorageException.class, () -> this.documentStorageFolderRestRepository.getMetadataOfAllFilesInFolderRecursively(pathToFolder));
+        verify(this.folderApi, times(1)).getMetadataOfAllFilesInFolderRecursively(pathToFolder);
     }
 
     @Test
