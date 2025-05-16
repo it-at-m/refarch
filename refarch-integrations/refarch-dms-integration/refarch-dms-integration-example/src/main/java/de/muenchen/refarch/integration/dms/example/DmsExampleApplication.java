63d1bf96ba1a87ae2e223374a2c4eddbefc3741a
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2025-05-16 08:25:33 +0200

    feat(dms): add example

---
+++ refarch-integrations/refarch-dms-integration/refarch-dms-integration-example/src/main/java/de/muenchen/refarch/integration/dms/example/DmsExampleApplication.java
@@ -0,0 +1,30 @@
+package de.muenchen.refarch.integration.dms.example;
+
+import de.muenchen.refarch.integration.dms.domain.exception.DmsException;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication()
+@RequiredArgsConstructor
+@EnableConfigurationProperties(DmsExampleProperties.class)
+@SuppressWarnings("PMD.UseUtilityClass")
+public class DmsExampleApplication {
+    private final ExampleDmsService exampleDmsService;
+    private final ApplicationContext context;
+
+    public static void main(final String[] args) {
+        SpringApplication.run(DmsExampleApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void testDms() throws IOException, DmsException {
+        this.exampleDmsService.testCreateDocument();
+        SpringApplication.exit(context);
+    }
+}
