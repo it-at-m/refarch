40469ad146cfb5855f6d47b23aa887d1b6360b18
Simon Hirtreiter <14137977+simonhir@users.noreply.github.com>
2025-05-16 10:18:13 +0200

    refact(dms)!: rm s3 (#547)
    * refact(dms): rm s3
    
    * feat(dms): add example
    
    * feat(dms): add example
    
    * fix(dms): properties validation
    
    * chore(dms): fix PMD warning
    
    * chore(integration): exclude examples from maven central push
    
    * chore(dms): cleanup
    
    Co-authored-by: Tobias Stadler <28538704+devtobi@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Tobias Stadler <28538704+devtobi@users.noreply.github.com>

--- refarch-integrations/refarch-dms-integration/refarch-dms-integration-core/src/test/java/de/muenchen/refarch/integration/dms/application/usecase/UpdateDocumentUseCaseTest.java
+++ refarch-integrations/refarch-dms-integration/refarch-dms-integration-core/src/test/java/de/muenchen/refarch/integration/dms/application/usecase/UpdateDocumentUseCaseTest.java
@@ -9,45 +9,39 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import de.muenchen.refarch.integration.dms.application.port.out.ListContentOutPort;
-import de.muenchen.refarch.integration.dms.application.port.out.LoadFileOutPort;
 import de.muenchen.refarch.integration.dms.application.port.out.UpdateDocumentOutPort;
 import de.muenchen.refarch.integration.dms.domain.exception.DmsException;
 import de.muenchen.refarch.integration.dms.domain.model.Content;
 import de.muenchen.refarch.integration.dms.domain.model.DocumentResponse;
 import de.muenchen.refarch.integration.dms.domain.model.DocumentType;
-import de.muenchen.refarch.integration.s3.client.exception.DocumentStorageException;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class UpdateDocumentUseCaseTest {
 
-    private final LoadFileOutPort loadFileOutPort = mock(LoadFileOutPort.class);
     private final ListContentOutPort listContentOutPort = mock(ListContentOutPort.class);
 
     private final UpdateDocumentOutPort updateDocumentOutPort = mock(UpdateDocumentOutPort.class);
 
-    private final UpdateDocumentUseCase updateDocumentUseCase = new UpdateDocumentUseCase(updateDocumentOutPort, listContentOutPort, loadFileOutPort);
+    private final UpdateDocumentUseCase updateDocumentUseCase = new UpdateDocumentUseCase(updateDocumentOutPort, listContentOutPort);
 
     @Test
-    void updateDocument() throws DmsException, DocumentStorageException {
+    void updateDocument() throws DmsException {
 
         final Content content = new Content("extension", "name", "content".getBytes());
 
-        final List<String> filepaths = List.of("path/content.pdf");
         final String docCoo = "documentCOO";
         final String user = "user";
         final List<String> fileCoos = List.of("contentCoo1", "contentCoo2");
 
-        when(this.loadFileOutPort.loadFiles(any())).thenReturn(List.of(content));
         when(this.listContentOutPort.listContentCoos(docCoo, user)).thenReturn(fileCoos);
 
         doNothing().when(updateDocumentOutPort).updateDocument(any(), any(), any(), any());
 
-        final DocumentResponse documentResponse = updateDocumentUseCase.updateDocument(docCoo, "user", DocumentType.EINGEHEND, filepaths);
+        final DocumentResponse documentResponse = updateDocumentUseCase.updateDocument(docCoo, "user", DocumentType.EINGEHEND, List.of(content));
 
         assertEquals(docCoo, documentResponse.documentCoo());
         assertEquals(fileCoos, documentResponse.contentCoos());
-        verify(this.loadFileOutPort, times(1)).loadFiles(filepaths);
         verify(this.updateDocumentOutPort, times(1)).updateDocument(docCoo, DocumentType.EINGEHEND, List.of(content), "user");
 
     }
