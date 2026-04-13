1fd0921e8ba7358953f57d94d9fa1d4dcfcb4d40
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2026-04-13 10:52:30 +0200

    chore(integrations): relocate to oss groupId

--- refarch-integrations/refarch-dms-integration/refarch-dms-integration-core/src/main/java/de/muenchen/refarch/integration/dms/domain/model/RequestContext.java
+++
@@ -1,8 +0,0 @@
-package de.muenchen.refarch.integration.dms.domain.model;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record RequestContext(@NotBlank String user,
-        String jobOe,
-        String jobPosition) {
-}
