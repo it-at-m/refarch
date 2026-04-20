4af0584184ac4817291bcff3e845094a9c72f112
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2026-04-20 10:45:48 +0200

    chore(gateway,integrations): validate all ConfigurationProperties

--- refarch-gateway/src/main/java/de/muenchen/oss/refarch/gateway/configuration/SecurityProperties.java
+++ refarch-gateway/src/main/java/de/muenchen/oss/refarch/gateway/configuration/SecurityProperties.java
@@ -2,16 +2,19 @@ package de.muenchen.oss.refarch.gateway.configuration;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import edu.umd.cs.findbugs.annotations.SuppressMatchType;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @Data
 @ConfigurationProperties("refarch.security")
+@Validated
 public class SecurityProperties {
     /**
      * List of url patterns excluded from csrf protection.
      */
     @SuppressFBWarnings(value = "EI_EXPOSE_REP", matchType = SuppressMatchType.EXACT)
-    private List<String> csrfWhitelisted = List.of();
+    @NotNull private List<String> csrfWhitelisted = List.of();
 }
