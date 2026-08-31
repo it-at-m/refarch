a363486cb736b1dac785756c31ba8b511a04b321
langehm <38280183+langehm@users.noreply.github.com>
2026-08-31 18:27:09 +0200

    :recycle: refactored test to use stub as backend

--- refarch-gateway/src/test/java/de/muenchen/oss/refarch/gateway/filter/GlobalRequestParameterPollutionFilterTest.java
+++ refarch-gateway/src/test/java/de/muenchen/oss/refarch/gateway/filter/GlobalRequestParameterPollutionFilterTest.java
@@ -1,9 +1,11 @@
 package de.muenchen.oss.refarch.gateway.filter;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static de.muenchen.oss.refarch.gateway.TestConstants.SPRING_TEST_PROFILE;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import de.muenchen.oss.refarch.gateway.OAuthSecurityMockConfiguration;
@@ -11,25 +13,21 @@ import de.muenchen.oss.refarch.gateway.configuration.SecurityProperties;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import java.util.Set;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
-import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
-import org.springframework.mock.web.server.MockServerWebExchange;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
+import org.wiremock.spring.EnableWireMock;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles(SPRING_TEST_PROFILE)
+@EnableWireMock
 @Import(OAuthSecurityMockConfiguration.class)
 @AutoConfigureWebTestClient
 class GlobalRequestParameterPollutionFilterTest {
@@ -63,33 +61,27 @@ class GlobalRequestParameterPollutionFilterTest {
         final String url = "/api/backend/testendpoint?parameter1=testdata_1&parameter2=testdata&parameter1=testdata_2";
 
         webTestClient.get().uri(url).exchange()
-                .expectStatus()
-                .isEqualTo(HttpStatus.BAD_REQUEST)
+                .expectStatus().isEqualTo(HttpStatus.BAD_REQUEST)
                 .expectBody()
                 .consumeWith(responseBody -> jsonResponseBody.append(
                         new String(Objects.requireNonNull(responseBody.getResponseBody()), StandardCharsets.UTF_8)));
+
         assertTrue(jsonResponseBody.toString().contains("\"message\":\"parameter pollution\""));
     }
 
     @Test
+    @WithMockUser
     void parameterPollutionAttack_WithMatchedWhitelist_Allows() {
         when(securityProperties.getParameterPollutionWhitelisted()).thenReturn(Set.of("parameter1"));
 
-        final MockServerHttpRequest request = MockServerHttpRequest.get("/api/backend/testendpoint")
-                .queryParam("parameter1", "testdata_1")
-                .queryParam("parameter2", "testdata")
-                .queryParam("parameter1", "testdata_2")
-                .build();
+        stubFor(get(urlPathEqualTo("/testendpoint"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())));
 
-        final ServerWebExchange exchange = MockServerWebExchange.from(request);
-        final GatewayFilterChain filterChain = mock(GatewayFilterChain.class);
-        when(filterChain.filter(exchange)).thenReturn(Mono.empty());
+        final String url = "/api/backend/testendpoint?parameter1=testdata_1&parameter2=testdata&parameter1=testdata_2";
 
-        final GlobalRequestParameterPollutionFilter filter = new GlobalRequestParameterPollutionFilter(securityProperties);
-
-        filter.filter(exchange, filterChain).block();
-
-        verify(filterChain).filter(exchange);
+        webTestClient.get().uri(url).exchange()
+                .expectStatus().isOk();
     }
 
 }
